762583d016d41cc4a1896ecc99f7d8d344ad411f
Heri Fiononana <heryfiononana19@gmail.com>
2023-11-13 14:21:25 +0300

    feat: create searchInput component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import SearchInput from "../../components/input/search";
+import { Box, IconButton } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
 function Home() {
-  return <div>Accueil</div>;
+  return (
+    <div>
+      Accueil
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-around",
+        }}
+      >
+        <SearchInput handleChange={() => {}} />
+        <IconButton>
+          <AddIcon />
+        </IconButton>
+      </Box>
+    </div>
+  );
 }
 
 export default Home;

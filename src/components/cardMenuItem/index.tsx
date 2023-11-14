3583d2c349a6dea97bcde7d9550d157e75447645
Heri Fiononana <heryfiononana19@gmail.com>
2023-11-14 10:47:17 +0300

    feat(home): create component cardAction

---
+++ src/components/cardMenuItem/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+// todo: add action in props
+interface CardActionItemProps {
+  title: string;
+  description: string;
+  backgroundImageSource: string;
+}
+
+function CardActionItem({
+  title,
+  description,
+  backgroundImageSource,
+}: CardActionItemProps) {
+  return (
+    <Box
+      sx={{
+        height: "100%",
+        marginTop: 0.5,
+        marginBottom: 0.5,
+        backgroundImage: `url(${backgroundImageSource})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+        backgroundSize: "cover",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "white",
+      }}
+    >
+      <Typography sx={{ fontWeight: "bold" }}>
+        {title.toLocaleUpperCase()}
+      </Typography>
+      <Typography>{description}</Typography>
+    </Box>
+  );
+}
+
+export default CardActionItem;

366bc9db9e508c53df002593346e2aae39c4fa7b
Heri Fiononana <heryfiononana19@gmail.com>
2023-11-15 10:33:23 +0300

    fix: use LocalizationProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import "./App.css";
 import { ThemeProvider } from "@mui/material/styles";
 import { useTheme } from "./provider/ThemeProvider";
 import RouterProvider from "./routes";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 function App() {
   const { theme } = useTheme();
 
   return (
-    <ThemeProvider theme={theme}>
-      <RouterProvider />
-    </ThemeProvider>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider />
+      </ThemeProvider>
+    </LocalizationProvider>
   );
 }
 

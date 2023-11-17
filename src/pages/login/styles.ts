3cec7286d0eea0d97a8f316c75a3f2f309dc87ff
Heri Fiononana <heryfiononana19@gmail.com>
2023-11-17 10:21:13 +0300

    feat(login): create login page

---
+++ src/pages/login/styles.ts
@@ -0,0 +1,36 @@
+import { makeStyles } from "@material-ui/styles";
+
+export default makeStyles({
+  container: {
+    height: "100vh",
+    paddingInline: 30,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    width: "100%",
+    textAlign: "center",
+    fontSize: "1.2rem",
+    padding: 10,
+  },
+  input: {
+    "& fieldset": {
+      border: "none",
+    },
+    backgroundColor: "white",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  submitButton: {
+    borderRadius: "25px",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+});

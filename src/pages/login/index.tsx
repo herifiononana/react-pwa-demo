8fdbee5e0caab0fa34f4cc474ede6ac895157212
Heri Fiononana <heryfiononana19@gmail.com>
2023-11-17 10:21:13 +0300

    feat(login): create login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,14 +1,34 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
+import { Box, Divider, Link, TextField, Typography } from "@mui/material";
+import useStyles from "./styles";
 
 function Login() {
   const navigate = useNavigate();
+  const classes = useStyles();
+
   return (
-    <div>
-      Login page
-      <Button onClick={() => navigate("/home")}>Login</Button>
-    </div>
+    <Box className={classes.container}>
+      <Box className={classes.headContainer}>
+        <Divider sx={{ width: "100%" }} />
+        <Typography className={classes.title}>Connexion</Typography>
+        <Divider sx={{ width: "100%" }} />
+      </Box>
+      <TextField placeholder="Adresse e-mail" className={classes.input} />
+      <TextField placeholder="Mot de passe" className={classes.input} />
+      <Button
+        variant="contained"
+        onClick={() => navigate("/home")}
+        fullWidth
+        className={classes.submitButton}
+      >
+        Se connecter
+      </Button>
+      <Link sx={{ fontSize: ".8rem", textDecoration: "none" }}>
+        Mot de passe oublier ?
+      </Link>
+    </Box>
   );
 }
 

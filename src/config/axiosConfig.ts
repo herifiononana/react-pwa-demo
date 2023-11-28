59ae7a8af7cf2f27fde8b41b530362b900d9b0ab
Heri Fiononana <heryfiononana19@gmail.com>
2023-11-28 20:25:26 +0300

    feat: reconfigure axios

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosConfig = axios.create({
   baseURL:
-    process.env.REACT_APP_BASE_URL || "https://votre-site-wordpress/wp-json",
-  timeout: 5000,
+    process.env.REACT_APP_BASE_URL ||
+    "https://stg-maisonbreuer-breuerdev.kinsta.cloud/wp-json/",
   headers: {
     // add other common header if necessary
     "Content-Type": "application/json",

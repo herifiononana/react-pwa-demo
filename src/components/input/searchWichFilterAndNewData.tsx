1290d899db952c8bed6f609c4edc71ee49fe0650
Heri Fiononana <heryfiononana19@gmail.com>
2023-11-29 20:44:33 +0300

    feat(customer): customize datagrid cell

---
+++ src/components/input/searchWichFilterAndNewData.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Box, TextField, Tooltip, IconButton } from "@mui/material";
+import TuneIcon from "@mui/icons-material/Tune";
+import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
+
+function SearchWichFilterAndNewData() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 2,
+      }}
+    >
+      <TextField
+        placeholder="Search Customers"
+        sx={{
+          "& fieldset": {
+            borderColor: "#888",
+          },
+        }}
+      />
+
+      <Tooltip title="Add new customer">
+        <IconButton>
+          <PersonAddAlt1Icon sx={{ color: "primary.main" }} />
+        </IconButton>
+      </Tooltip>
+      <IconButton>
+        <TuneIcon sx={{ color: "primary.main" }} />
+      </IconButton>
+    </Box>
+  );
+}
+
+export default SearchWichFilterAndNewData;

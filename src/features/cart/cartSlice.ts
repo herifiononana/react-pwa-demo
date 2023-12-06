82cf6e27ac1abdc201de136810c3b2b75a1e6b3b
Heri Fiononana <heryfiononana19@gmail.com>
2023-12-06 09:18:01 +0300

    feat(cart): create cart reducer

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,22 +1,48 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Product } from "../../services/product/productService";
 
 interface CartItem {
   // todo: Define the structure of basket items
+  status: "idle" | "loading" | "succeeded" | "failed";
+  data: Product[];
+  error: string | null;
 }
 
-const initialState: CartItem[] = [];
+const initialState: CartItem = {
+  status: "idle",
+  data: [],
+  error: null,
+};
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addToCart: (state, action: PayloadAction<CartItem>) => {
-      state.push(action.payload);
+    // addToCart: (state, action: PayloadAction<CartItem>) => {
+    //   state.push(action.payload);
+    // },
+    addToCartStart: (state) => {
+      state = { ...state, status: "loading" };
+    },
+    addToCartSucces: (state, action: PayloadAction<Product>) => {
+      state = {
+        ...state,
+        status: "idle",
+        data: [...state.data, action.payload],
+      };
+    },
+    addToCartFailure: (state, action: PayloadAction<string>) => {
+      state = {
+        ...state,
+        status: "failed",
+        error: action.payload,
+      };
     },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
-export const getCart = (state: { cart: CartItem[] }) => state.cart;
+export const { addToCartStart, addToCartSucces, addToCartFailure } =
+  cartSlice.actions;
+// export const getCart = (state: { cart: CartItem[] }) => state.cart;
 
 export default cartSlice.reducer;

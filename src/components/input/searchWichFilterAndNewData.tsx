aa3f6f543bd8140333adbad2ae2212897ca610bf
Heri Fiononana <heryfiononana19@gmail.com>
2023-12-08 20:50:34 +0300

    feat(customers): set up search

--- src/components/input/searchWichFilterAndNewData.tsx
+++ src/components/input/searchWichFilterAndNewData.tsx
@@ -1,13 +1,34 @@
 import React from "react";
-import { Box, TextField, Tooltip, IconButton } from "@mui/material";
+import { Box, Tooltip, IconButton } from "@mui/material";
 import TuneIcon from "@mui/icons-material/Tune";
 import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
+import Select from "react-select";
+import { Customer } from "../../services/customer/customerService";
+import { formatCustomerOption } from "../../pages/products/cartUtil";
+import COLORS from "../../styles/color";
 
+export interface SearchOption {
+  value: string;
+  label: string;
+}
+
+interface SearchAndFilterProps {
+  data: Customer[];
+  setSelectedCustomer: React.Dispatch<
+    React.SetStateAction<SearchOption | null>
+  >;
+  setOpenModalCustomer: React.Dispatch<React.SetStateAction<boolean>>;
+}
 function SearchWichFilterAndNewData({
-  addNewData,
-}: {
-  addNewData: () => void;
-}) {
+  data,
+  setSelectedCustomer,
+  setOpenModalCustomer,
+}: SearchAndFilterProps) {
+  const options = formatCustomerOption(data);
+
+  const handleChange = (selectedOption: any) => {
+    setSelectedCustomer(selectedOption as SearchOption);
+  };
   return (
     <Box
       sx={{
@@ -17,17 +38,34 @@ function SearchWichFilterAndNewData({
         marginBottom: 2,
       }}
     >
-      <TextField
-        placeholder="Search Customers"
-        sx={{
-          "& fieldset": {
-            borderColor: "#888",
-          },
-        }}
-      />
+      <Box sx={{ width: "100%", zIndex: 10000 }}>
+        <Select
+          className="basic-single"
+          classNamePrefix="select"
+          onChange={handleChange}
+          isClearable={true}
+          isRtl={false}
+          isSearchable={true}
+          name="product"
+          options={options}
+          placeholder="Search customer"
+          styles={{
+            control: (baseStyles, state) => ({
+              ...baseStyles,
+              borderColor: "#888",
+              width: "100%",
+            }),
+            option: () => ({
+              fontSize: ".9rem",
+              margin: 20,
+              color: COLORS.text.main,
+            }),
+          }}
+        />
+      </Box>
 
       <Tooltip title="Add new customer">
-        <IconButton onClick={addNewData}>
+        <IconButton onClick={() => setOpenModalCustomer(true)}>
           <PersonAddAlt1Icon sx={{ color: "primary.main" }} />
         </IconButton>
       </Tooltip>
